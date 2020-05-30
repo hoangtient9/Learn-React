60448e571d740f7125fe2d25c2d0c5740a70ae54
hoangtjent9 <hoangtjent9@gmail.com>
2020-05-30 14:42:40 +0700

    Add Person method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi Tien</h1>
       <p>This is really working!</p>
+      <Person />
     </div>
 
     // React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, 'Hi, I\'m Tien'))
