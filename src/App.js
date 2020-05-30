29c42e38185123c7fe10eb570e9f95339618fadb
hoangtjent9 <hoangtjent9@gmail.com>
2020-05-30 19:32:41 +0700

    Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = () => {
+class App extends Component {
 
-  const [personsState, setPersonsState] = useState({
+  state = {
     persons: [
       {name: 'Tien', age: 22},
       {name: 'Luis', age: 30},
       {name: 'Max', age: 25}
     ],
-    otherState: 'some other value'
-  });
+    otherState: 'some other value',
+    showPersons: false
+  };
 
-  console.log(personsState);
-
-  const switchNameHandler = (name) => {
-    setPersonsState({
+  switchNameHandler = (name) => {
+    this.setState({
       persons: [
         {name: name, age: 22},
         {name: 'Luis', age: 30},
         {name: 'Max', age: 27}
-      ],
-      otherState: 'some other value'
+      ]
     })
   };
 
-  const nameChangedHandler = (event) => {
-    setPersonsState({
+  nameChangedHandler = (event) => {
+    this.setState({
       persons: [
         {name: 'Tien', age: 22},
         {name: event.target.value, age: 30},
         {name: 'Max', age: 27}
-      ],
-      otherState: 'some other value'
+      ]
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hi Tien</h1>
-      <p>This is really working!</p>
-      <button onClick={() => switchNameHandler('Hoang')}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={switchNameHandler.bind(this, 'Hoa')} change={nameChangedHandler}>My Hobbies: Racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
+  tooglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
 
-    // React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, 'Hi, I\'m Tien'))
-  );
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      font: 'inherit'
+    }
+    return (
+      <div className="App">
+        <h1>Hi Tien</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.tooglePersonsHandler}>Switch Name</button>
+        {this.state.showPersons ? <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Hoa')} change={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div> : null
+        }
+      </div>
+  
+      // React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, 'Hi, I\'m Tien'))
+    );
+  }
 }
 
 export default App;
