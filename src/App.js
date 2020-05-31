cf925c9702f4cf86625494e90374f08021663011
hoangtjent9 <hoangtjent9@gmail.com>
2020-06-01 00:23:28 +0700

    Use Error Boundaries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -45,7 +46,14 @@ class App extends Component {
         <div>
           {
             this.state.persons.map((person, index) => {
-              return <Person name={person.name} age={person.age} key={person.id} click={() => this.deletePersonHandler(index)} change={(event) => this.nameChangedHandler(event, person.id)} />
+              return <ErrorBoundary key={person.id}>
+                <Person
+                  name={person.name} 
+                  age={person.age} 
+                  click={() => this.deletePersonHandler(index)} 
+                  change={(event) => this.nameChangedHandler(event, person.id)} 
+                />
+              </ErrorBoundary>
             })
           }
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,9 @@ import React from "react";
 import classes from './Person.css';
 
 const person = (props) => {
-
+  if (Math.random() >= 0.7) {
+    throw new Error('Something went wrong!')
+  }
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
